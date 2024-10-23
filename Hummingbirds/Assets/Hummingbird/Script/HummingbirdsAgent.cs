bf70768faf526aa7f88aa6b0f4d6c8d8a62f8075
simonl <simon.lesly@yahoo.com>
2024-10-22 22:14:11 -0400

    HummingbirdsAgent_Variables_accesseurs_partie_2

--- Hummingbirds/Assets/Hummingbird/Script/HummingbirdsAgent.cs
+++ Hummingbirds/Assets/Hummingbird/Script/HummingbirdsAgent.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
 using UnityEngine;
 
 /// <summary>
@@ -24,5 +27,34 @@ public class HummingbirdAgent : Agent
 
     [Tooltip("Whether this is training mode or gameplay mode")]
     public bool trainingMode;
+
+    // The rigidbody of the agent
+    new private Rigidbody rigidbody;
+
+    // The flower area that the agent is in
+    private FlowerArea flowerArea;
+
+    // The nearest flower to the agent
+    private Flower nearestFlower;
+
+    // Allows for smoother pitch changes
+    private float smoothPitchChange = 0f;
+
+    // Allows for smoother yaw changes
+    private float smoothYawChange = 0f;
+
+    // Maximum angle that the bird can pitch up or down
+    private const float MaxPitchAngle = 80f;
+
+    // Maximum distance from the beak tip to accept nectar collision
+    private const float BeakTipRadius = 0.008f;
+
+    // Whether the agent is frozen (intentionally not flying)
+    private bool frozen = false;
+
+    /// <summary>
+    /// The amount of nectar the agent has obtained this episode
+    /// </summary>
+    public float NectarObtained { get; private set; }
 
 }
